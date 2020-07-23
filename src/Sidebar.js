83edf7765cfea44754a36d5b85636e3981f67e58
geimaj <16000925@vcconnect.co.za>
2020-07-23 16:32:27 +0200

    replace selected links with new entry

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,19 @@
 import React, { useEffect, useState } from "react";
-import { CheckboxField } from "@contentful/forma-36-react-components";
+import {
+	FieldGroup,
+	CheckboxField,
+	Button,
+	ValidationMessage,
+} from "@contentful/forma-36-react-components";
 
 function Sidebar({ sdk }) {
 	const [linksToUpdate, setLinksToUpdate] = useState([]);
 	const [linkedEntries, setLinkedEntries] = useState([]);
+	const [linkedContentTypes, setLinkedContentTypes] = useState({});
+	const [selectAll, setSelectAll] = useState(false);
+	const [validSelection, setValidSeclection] = useState(false);
+
+	console.log(sdk);
 
 	const getLinkedEntries = async () => {
 		const entries = await sdk.space.getEntries({
@@ -13,16 +23,26 @@ function Sidebar({ sdk }) {
 		if (entries.total > 0) {
 			const displayFields = {};
 
+			//find the display field for each entry and add as a property
 			const entriesWithTitle = await Promise.all(
 				entries.items.map(async (entry) => {
 					const contentTypeId = entry.sys.contentType.sys.id;
+					// get the content-type if we don't already know
+					// the displayField for this content-type
 					if (!displayFields[contentTypeId]) {
 						const responseEntry = await sdk.space.getContentType(
 							contentTypeId
 						);
 						displayFields[contentTypeId] =
 							responseEntry.displayField;
+
+						// store the content type in state so later
+						// we can update the link field
+						const newContentTypes = { ...linkedContentTypes };
+						newContentTypes[contentTypeId] = responseEntry;
+						setLinkedContentTypes(newContentTypes);
 					}
+					// get the locale value for the display field and add to the entry
 					const title = entry.fields[displayFields[contentTypeId]];
 					const titleLocales = Object.keys(title);
 					return {
@@ -36,9 +56,99 @@ function Sidebar({ sdk }) {
 		}
 	};
 
+	const handlePublish = async () => {
+		if (
+			linksToUpdate.length > 0 &&
+			linksToUpdate.length < linkedEntries.length
+		) {
+			//create a new entry with the current unpublished data
+			const entryId = sdk.entry.getSys().id;
+			const entry = await sdk.space.getEntry(entryId);
+			const contentTypeId = entry.sys.contentType.sys.id;
+			const unpublishedFields = entry.fields;
+
+			// TODO: edit title field to relate to this entry
+			const newEntryDraft = await sdk.space.createEntry(contentTypeId, {
+				fields: { ...unpublishedFields },
+			});
+
+			//publish new entry
+			const newEntry = await sdk.space.publishEntry(newEntryDraft);
+
+			//link each entry to update with the new entry
+			linksToUpdate.forEach(async (linkedEntryId) => {
+				const linkedEntry = linkedEntries.filter(
+					(link) => link.sys.id === linkedEntryId
+				)[0];
+
+				const newFields = { ...linkedEntry.fields };
+
+				// find the field that links to this entry
+				// and replace it with a link to newEntry
+
+				Object.keys(linkedEntry.fields).forEach((key) => {
+					const field = linkedEntry.fields[key];
+					// loop through the locales for each field
+					Object.keys(field).forEach((locale) => {
+						const fieldLocale = field[locale];
+						// check if field is an array
+						if (Array.isArray(fieldLocale)) {
+							fieldLocale.forEach((item, index) => {
+								//check if item is a link
+								if (item.sys) {
+									//check if this is the link we should replace
+									if (item.sys.id === entryId) {
+										newFields[key][locale][index].sys.id =
+											newEntry.sys.id;
+									}
+								}
+							});
+						} else {
+						}
+					});
+				});
+
+				// update the linked entry
+				const updatedLinkedEntry = await sdk.space.updateEntry({
+					...linkedEntry,
+					fields: newFields,
+				});
+				console.log("updated", updatedLinkedEntry);
+			});
+
+			//TODO: revert changes on current entry
+			// console.log("orig", entry);
+			// const snapshots = await sdk.space.getEntrySnapshots(entryId);
+			// console.log("snapshots", snapshots);
+		} else {
+			// if the user selected all the linked entries then we can
+			// leave the existing links and update as normal
+		}
+	};
+
+	useEffect(() => {
+		setValidSeclection(
+			linkedEntries.length <= 0 ||
+				(linkedEntries.length > 0 && linksToUpdate.length > 0)
+		);
+	}, [linkedEntries, linksToUpdate]);
+
+	//find all the entries that link to this entry
 	useEffect(() => {
 		getLinkedEntries();
-	}, [getLinkedEntries]);
+	}, []);
+
+	//keep `select all` in sync
+	useEffect(() => {
+		if (
+			linksToUpdate.length > 0 &&
+			linksToUpdate.length === linkedEntries.length
+		) {
+			setSelectAll(true);
+		} else {
+			setSelectAll(false);
+		}
+	}, [linksToUpdate]);
 
 	let result = (
 		<div>
@@ -47,54 +157,81 @@ function Sidebar({ sdk }) {
 	);
 
 	if (linkedEntries.length > 0) {
-		//add update listener to each field
-		// Object.keys(sdk.entry.fields).forEach((key) => {
-		// 	const field = sdk.entry.fields[key];
-		// 	console.log("got field", field);
-		// 	field.onValueChanged((value) => {
-		// 		console.log("field " + key + " changed!!!");
-		// 		console.log(value);
-		// 	});
-		// });
-
 		result = (
 			<div>
-				<h1>Warning</h1>
-				<h4>This entry is used in more than one place</h4>
-				<h5>
-					Select the places you inted to be affected by updating this
-					entry
-				</h5>
-				{linkedEntries.map((entry, index) => {
-					return (
-						<CheckboxField
-							key={entry.sys.id}
-							labelText={entry.displayField}
-							helpText={entry.sys.contentType.sys.id}
-							name={entry.sys.id}
-							id={entry.sys.id}
-							checked={linksToUpdate.includes(entry.sys.id)}
-							onChange={(e) => {
-								if (e.target.checked) {
-									setLinksToUpdate([
-										...linksToUpdate,
-										entry.sys.id,
-									]);
-								} else {
-									setLinksToUpdate(
-										linksToUpdate.filter(
-											(id) => id !== entry.sys.id
-										)
-									);
-								}
-							}}
-						/>
-					);
-				})}
+				<h1>Warning:</h1>
+				<h5>This entry is used in more than one place</h5>
+
+				<FieldGroup>
+					<CheckboxField
+						key={"all"}
+						labelText={"Select all"}
+						name={"all"}
+						id={"all"}
+						checked={selectAll}
+						onChange={(e) => {
+							if (e.target.checked) {
+								setLinksToUpdate(
+									linkedEntries.map((entry) => entry.sys.id)
+								);
+								setSelectAll(true);
+							} else {
+								setSelectAll(false);
+								setLinksToUpdate([]);
+							}
+						}}
+					/>
+					{linkedEntries.map((entry, index) => {
+						return (
+							<CheckboxField
+								key={entry.sys.id}
+								labelText={entry.displayField}
+								helpText={entry.sys.contentType.sys.id}
+								name={entry.sys.id}
+								id={entry.sys.id}
+								checked={linksToUpdate.includes(entry.sys.id)}
+								onChange={(e) => {
+									if (e.target.checked) {
+										setLinksToUpdate([
+											...linksToUpdate,
+											entry.sys.id,
+										]);
+									} else {
+										setLinksToUpdate(
+											linksToUpdate.filter(
+												(id) => id !== entry.sys.id
+											)
+										);
+									}
+								}}
+							/>
+						);
+					})}
+				</FieldGroup>
+				{!validSelection ? (
+					<ValidationMessage>
+						Select the places you inted to be affected by updating
+						this entry
+					</ValidationMessage>
+				) : null}
 			</div>
 		);
 	}
-	console.log("linkedEntries: ", linkedEntries);
-	return <div>{result}</div>;
+
+	return (
+		<div>
+			{result}
+
+			<Button
+				buttonType="positive"
+				onClick={() => {
+					handlePublish();
+				}}
+				disabled={!validSelection}
+			>
+				Publish
+			</Button>
+		</div>
+	);
 }
 export default Sidebar;
--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import "./index.css";
 import { init, locations } from "contentful-ui-extensions-sdk";
 import Sidebar from "./Sidebar";
 import Config from "./Config";
+import "@contentful/forma-36-react-components/dist/styles.css";
+
 init((sdk) => {
 	const root = document.getElementById("root");
-	console.log("init");
 
 	if (sdk.location.is(locations.LOCATION_APP_CONFIG)) {
 		render(<Config sdk={sdk} />, root);
-	} else {
+	} else if (sdk.location.is(locations.LOCATION_ENTRY_SIDEBAR)) {
 		render(<Sidebar sdk={sdk} />, root);
 		sdk.window.startAutoResizer();
 	}
